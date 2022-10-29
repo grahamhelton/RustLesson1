e685f03513b48a68d124b0a8510ffe3e3e4d4738
Graham Helton <contact@grahamhelton.com>
2022-10-28 21:25:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rust chat server 
+This is a chat server written in rust using this tutorial https://www.youtube.com/watch?v=Iapc-qGTEBQ
+
+Not going to lie, a lot of this is over my head but I learned a lot
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+// Chat server written in rust using this tutorial https://www.youtube.com/watch?v=Iapc-qGTEBQ
+// Not going to lie, a lot of this is over my head but I learned a lot
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+
+#[tokio::main]
+async fn main() {
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx,_rx) = broadcast::channel(10);
+    //Client Connection
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader,mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            //let mut line = String::names[0]; 
+            let mut line = String::new();
+
+            loop { 
+                tokio::select!{
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(),addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg,other_addr) = result.unwrap();
+                        if addr != other_addr{
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+
+                }
+
+            }
+        });
+    }
+}
